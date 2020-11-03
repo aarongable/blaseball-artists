1bf24e62f76b1cda1e0e3e175cfe7e201845d4a6
Aaron Gable <aaron@aarongable.com>
2020-11-03 09:57:18 -0800

    Modifications for use for artist commissions

--- app.js
+++ app.js
@@ -18,17 +18,17 @@ const { GoogleSpreadsheet } = require("google-spreadsheet");
 const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
 doc.useApiKey(process.env.API_KEY);
 const NodeCache = require("node-cache");
-var teamCache = new NodeCache();
+var artistCache = new NodeCache();
 
 app.get("/",async (req, res)=>{
-    res.render("index.html", {teams: teamCache.get("rows")});
+    res.render("index.html", {artists: artistCache.get("rows")});
 });
 
 async function updateCache(){
     if(process.env.NODE_ENV == "DEV") console.log(new Date()+": Updated Cache");
     let sheet = doc.sheetsByIndex[0];
     let rows = await sheet.getRows({offset:0});
-    teamCache.set("rows",rows.map(r => {return {name: r.Name, status: r.Status.toLowerCase(), emoji: r.Emoji, desc: r.Description?.replace(/\n/g,"<br>")??"Description not set"};}));
+    artistCache.set("rows",rows.map(r => {return {name: r.Name, status: r.Status.toLowerCase(), emoji: r.Emoji, link: r.Link, desc: r.Contact?.replace(/\n/g,"<br>")??"Description not set"};}));
 }
 
 setInterval(updateCache,30000);
@@ -36,7 +36,7 @@ setInterval(updateCache,30000);
 (async ()=>{
     await doc.loadInfo();
     await updateCache();
-    let port = process.env.PORT ?? 8000;
+    let port = process.env.PORT ?? 443;
     app.listen(port, () => {
         console.log("Ready on",port);
     });
